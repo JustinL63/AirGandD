6b2f70adb3a8ed4798579c3363b56a8362831b1b
Ed Courtney <edcourtney74@gmail.com>
2018-12-23 19:05:24 -0600

    Music dummy page created

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -21,9 +21,9 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
+  app.get("/music", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
+      res.render("music", {
         example: dbExample
       });
     });

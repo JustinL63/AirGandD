47b680d23c89340e6ab246dc43009a3c7c5785ca
Ed Courtney <edcourtney74@gmail.com>
2018-12-24 10:59:16 -0600

    Login and Create Account pages set up.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,14 +3,20 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("index");
+  });
+  
+  // Load login page
+  app.get("/login", function(req, res) {
+    res.render("login");
+  });
+  
+  // Load create page
+  app.get("/create", function(req, res) {
+    res.render("create");
   });
 
+  // Load dashboard page and data
   app.get("/dashboard", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("dashboard", {
@@ -20,14 +26,12 @@ module.exports = function(app) {
     });
   });
 
-  // Load example page and pass in an example by id
+  // Load music page (initial visit, no data) 
   app.get("/music", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("music", {
-        example: dbExample
-      });
-    });
+    res.render("music");
   });
+
+
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {

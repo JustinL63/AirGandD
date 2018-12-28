2c1298df3e4a7090a107d0ee52061a8565a3306a
Justin Lamb <lambjg63@gmail.com>
2018-12-28 13:40:05 -0500

    Generic Routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,3 +22,141 @@ module.exports = function(app) {
     });
   });
 };
+// Get all data from users database
+app.get("/user db location", function(req, res) {
+  db.Post.findAll({})
+    .then(function(userDB) {
+      res.json(userDB);
+    });
+});
+
+app.get("/api/music/remaining", function(req, res) {
+  db.Post.findAll({
+    //function to get all information where a user has not interacted with the data.
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});
+
+app.get("/api/music/nextup", function(req, res) {
+  db.Post.findAll({
+    //function to get all the albums the user has marked as interested in.
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});
+
+app.get("/api/music/listened", function(req, res) {
+  db.Post.findAll({
+    //function to get all the music the user has marked as listen to.
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});
+
+app.get("/api/music/nextup", function(req, res) {
+  db.Post.findAll({
+    //function to get all the albums the user has marked as interested in.
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});app.get("/api/music/all", function(req, res) {
+  db.Post.findAll({
+    //function to get all of the music from the original music list, does not require user input
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});
+
+app.get("/api/films/remaining", function(req, res) {
+  db.Post.findAll({
+    //function to get all information where a user has not interacted with the data.
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});
+
+app.get("/api/films/nextup", function(req, res) {
+  db.Post.findAll({
+    //function to get all the movies the user has marked as interested in.
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});
+
+app.get("/api/films/listened", function(req, res) {
+  db.Post.findAll({
+    //function to get all the movies the user has marked as listen to.
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});
+
+app.get("/api/films/nextup", function(req, res) {
+  db.Post.findAll({
+    //function to get all the movies the user has marked as interested in.
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});app.get("/api/films/all", function(req, res) {
+  db.Post.findAll({
+    //function to get all of the movies from the original music list, does not require user input
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});
+
+app.get("/api/books/remaining", function(req, res) {
+  db.Post.findAll({
+    //function to get all information where a user has not interacted with the data.
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});
+
+app.get("/api/books/nextup", function(req, res) {
+  db.Post.findAll({
+    //function to get all the albums the user has marked as interested in.
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});
+
+app.get("/api/books/listened", function(req, res) {
+  db.Post.findAll({
+    //function to get all the music the user has marked as listen to.
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});
+
+app.get("/api/books/nextup", function(req, res) {
+  db.Post.findAll({
+    //function to get all the albums the user has marked as interested in.
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});app.get("/api/books/all", function(req, res) {
+  db.Post.findAll({
+    //function to get all of the music from the original music list, does not require user input
+  })
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+});
+
+
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,53 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
       });
     });
   });
+  // Route to generic login page
+  app.get("/login", function (req, res) {
+    res.render("login", {
 
-  app.get("/dashboard", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    })
+  });
+  // Route to generic create page
+  app.get("/create", function (req, res) {
+    res.render("create", {
+
+    })
+  });
+  // Route to generic dashboard page
+  app.get("/dashboard", function (req, res) {
+    res.render("dashboard", {
+
+    })
+  });
+  // Route to generic music page
+  app.get("/music", function (req, res) {
+    res.render("music", {
+      //Needs to show next 3 items in the NextUp database
+    })
+  });
+  // Route to generic movies page
+  app.get("/movies", function (req, res) {
+    res.render("movies", {
+      //Needs to show next 3 items in the NextUp database
+    })
+  });
+  // Route to generic books page
+  app.get("/books", function (req, res) {
+    res.render("books", {
+      //Needs to show next 3 items in the NextUp database
+    })
+  });
+  app.get("/dashboard", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("dashboard", {
         msg: "Welcome!",
         examples: dbExamples
@@ -21,8 +56,8 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/music", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/music", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("music", {
         example: dbExample
       });
@@ -30,7 +65,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };

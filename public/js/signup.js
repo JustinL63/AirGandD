3809cc59bef6bc3d9db6e7d2962ce841610ae687
Arline <darline711@gmail.com>
2019-01-01 15:26:26 -0500

    got html to post above the form whenever someone doesnt fillout the form, passwords dont match up with emails, or if the user name/email has already been taken

--- public/js/signup.js
+++ public/js/signup.js
@@ -16,21 +16,20 @@ $(document).ready(function() {
         name: nameInput.val().trim(),
         userName: userNameInput.val()
       };
-
+      
       // Check to make sure all form fields have been filled out
       if (!userData.email || !userData.password || !userData.name || !userData.userName) {
-        alert("please fill out all forms");
+        document.getElementById("notFilled").innerHTML = "Please fill out all forms"
         return;
       }
-      // Once all fields have been filled (and all values are unique), the form is emptied
+      
+      // Once all fields have been filled (and email and user name values are unique), the form is emptied
       signUpUser(userData.email, userData.password, userData.name, userData.userName);
       emailInput.val("");
       passwordInput.val("");
       nameInput.val("");
       userNameInput.val("")
     });
-  
-    // Posts user info to the signup info api
     function signUpUser(email, password, name, userName) {
       $.post("/api/signup", {
         email: email,
@@ -38,13 +37,17 @@ $(document).ready(function() {
         name: name,
         userName: userName
       }).then(function(data) {
-        window.location.replace(data)
-        // throws error
-      }).catch(logInErr);
-
-      function logInErr(err){
-        $("#alert .msg").text(err.responseJSON);
-        $("#alert").fadeIn(500);
+        checkInfo(emailInput, userNameInput);
+      }).catch(function(err){
+        console.log(err);
+      });
+      function checkInfo(data, emailInput, userNameInput){
+        if (emailInput == data.email || userNameInput == data.userName){
+          document.getElementById("alreadyTaken").innerHTML = "Email or Username already taken. Please enter different information";
+          return;
+        } else{
+          window.location.replace(data)
+        }
       }
     }
   });

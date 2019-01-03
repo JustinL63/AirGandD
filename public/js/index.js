32380f90d96298f3ddf9058047ef49e313014741
Arline <darline711@gmail.com>
2019-01-03 13:12:25 -0500

    got funcitional search bars on the pages that show the entire databse tables

--- public/js/index.js
+++ public/js/index.js
@@ -23,25 +23,25 @@ $(document).ready(function () {
   // Remaining List  
   $("#view-all").on("click", function (event) {
     event.preventDefault();
-    
+
     // Retrieve page section info 
     var pageID = $("body").attr("data-content");
-    
+
     // Run switch statement to determine which page to go to. HTML route runs the db query on page load
     switch (pageID) {
       case "music":
         window.location.href = "/music";
         break;
-        
-        case "movies":
+
+      case "movies":
         window.location.href = "/movies";
         break;
 
-        case "books":
+      case "books":
         window.location.href = "/books";
         break;
 
-        default:
+      default:
         console.log("Something is wrong.")
     }
   })
@@ -60,7 +60,7 @@ $(document).ready(function () {
           window.location.href = "/music/nextup";
         })
         break;
-      
+
       case "movies":
         $.get("/movies/nextup", function (data) {
           window.location.href = "/movies/nextup";
@@ -84,7 +84,7 @@ $(document).ready(function () {
 
     // Retrieve page section info 
     var pageID = $("body").attr("data-content");
-    
+
     // Run switch statement to determine correct get request
     switch (pageID) {
       case "music":
@@ -92,31 +92,31 @@ $(document).ready(function () {
           window.location.href = "/music/completed";
         })
         break;
-      
-        case "movies":
+
+      case "movies":
         $.get("/movies/completed", function (data) {
           window.location.href = "/movies/completed";
         })
         break;
 
-        case "books":
+      case "books":
         $.get("/books/completed", function (data) {
           window.location.href = "/books/completed";
         })
         break;
 
-        default:
+      default:
         console.log("Something is wrong.")
     }
   })
-    
+
   // Full database
   $("#full-db").on("click", function (event) {
     event.preventDefault();
 
     // Retrieve page section info 
     var pageID = $("body").attr("data-content");
-    
+
     // Run switch statement to determine correct get request
     switch (pageID) {
       case "music":
@@ -124,24 +124,24 @@ $(document).ready(function () {
           window.location.href = "/music/full";
         })
         break;
-      
-        case "movies":
+
+      case "movies":
         $.get("/movies/full", function (data) {
           window.location.href = "/movies/full";
         })
         break;
 
-        case "books":
+      case "books":
         $.get("/books/full", function (data) {
           window.location.href = "/books/full";
         })
         break;
 
-        default:
+      default:
         console.log("Something is wrong.")
     }
   })
-    
+
   // REFRESH - To update list on Remaining view  
   $("#refresh").on("click", function (event) {
     event.preventDefault();
@@ -154,7 +154,7 @@ $(document).ready(function () {
     event.preventDefault();
     location.reload();
   })
-// ================================================================================
+  // ================================================================================
 
   // CLICK FUNCTIONS FOR USER-DB INTERACTION IN VIEW REMAINING/FULL======================
   // Function to run when any db-interaction button is clicked the first time
@@ -187,10 +187,10 @@ $(document).ready(function () {
 
     // Grab item ID
     itemID = $(this).attr("id");
-    
+
     // Fade row, then remove it from table display    
-    $(this).closest("tr").fadeOut(500, function() {
-      $(this).closest("tr").empty() 
+    $(this).closest("tr").fadeOut(500, function () {
+      $(this).closest("tr").empty()
     });
 
     // Grab data-page value
@@ -221,10 +221,10 @@ $(document).ready(function () {
 
     // Grab item ID
     itemID = $(this).attr("id");
-    
+
     // Fade row, then remove it from table display    
-    $(this).closest("tr").fadeOut(500, function() {
-      $(this).closest("tr").empty() 
+    $(this).closest("tr").fadeOut(500, function () {
+      $(this).closest("tr").empty()
     });
 
     // Grab data-page value
@@ -255,10 +255,10 @@ $(document).ready(function () {
 
     // Grab item ID
     itemID = $(this).attr("id");
-  
+
     // Fade row, then remove it from table display    
-    $(this).closest("tr").fadeOut(500, function() {
-      $(this).closest("tr").empty() 
+    $(this).closest("tr").fadeOut(500, function () {
+      $(this).closest("tr").empty()
     });
 
     // Grab data-page value
@@ -309,7 +309,7 @@ $(document).ready(function () {
     }).catch(function (err) {
       console.log(err);
     });
-  }  
+  }
 
   // LISTENED TO BUTTON
   $(".btn-nx-completed").on("click", function (event) {
@@ -317,12 +317,12 @@ $(document).ready(function () {
 
     // Grab ID of NextUp record
     nextID = $(this).attr("id");
-    
+
     // Check to see if button is pressed from main view or sidebar/dashboard
     if ($(this).attr("data-site")) {
       // If data-site is found, that means it's located in the sidebar/dashboard and the .item div should be hidden
       $(this).closest(".item").fadeOut();
-  
+
     } else
       // It's in the main view and the buttons should be removed
       $(this).closest('td').empty();
@@ -362,11 +362,11 @@ $(document).ready(function () {
     if ($(this).attr("data-site")) {
       // If data-site is found, that means it's located in the sidebar and the .item div should be hidden
       $(this).closest(".item").fadeOut();
-  
+
     } else
       // It's in the main view and the buttons should be removed
       $(this).closest('td').empty();
-    
+
     // Grab data-page value
     var type = $(this).attr("data-page");
 
@@ -388,4 +388,23 @@ $(document).ready(function () {
         console.log("Something is wrong.")
     }
   })
+//   function musicSearchBar() {
+//     var input = document.getElementById("serachInput");
+//     var filter = input.value.toLowerCase;
+//     var tbody = document.getElementByTagName("tbody")
+//     var tr = document.getElementByTagName("tr")
+//     var artist = document.getElementById("artist")
+//     for (var i = 0; i < tr.length; i++) {
+//       var td = tr[i].getElementsByTagName("td")[0];
+//       if (td) {
+//         var txtValue = artist;
+//         if (txtValue.toLowerCase().indexOf(filter) > -1) {
+//           tr[i].style.display = "";
+//         } else {
+//           tr[i].style.display = "none";
+//         }
+//       }
+//     }
+//   }
+// musicSearchBar();
 });

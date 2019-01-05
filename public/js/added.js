3bb12d0dcc79570ad2b9815b4b022702e8c08d90
Ed Courtney <edcourtney74@gmail.com>
2019-01-05 17:02:18 -0500

    User-added view showing all added items not interacted with. Interactive buttons working.

--- public/js/added.js
+++ public/js/added.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
     // GLOBAL VARIABLE
     // Variable to save userID
     var userID;
@@ -9,6 +8,7 @@ $(document).ready(function () {
         userID = data.id;
     })
 
+    // CLICK FUNCTIONS WHEN NEW ITEM SUBMITTED=================================
     $("#btn-album").on("click", function (event) {
         event.preventDefault();
         var addedAlbum = $("#albumTitle");
@@ -49,7 +49,7 @@ $(document).ready(function () {
         var addedMovie = $("#movieTitle");
         var addedYear = $("#movieYear");
         var addedRating = $("#movieRating");
-        
+
         var movieAdded = {
             title: addedMovie.val().trim(),
             year: addedYear.val().trim(),
@@ -70,7 +70,7 @@ $(document).ready(function () {
         $("#btn-movie").modal("hide");
 
         function addMovie(title, year, rating) {
-            $.post("/movies/addedMovies", {
+            $.post("/movies/addedmovies", {
                 user_id: userID,
                 title: title,
                 year: year,
@@ -85,7 +85,7 @@ $(document).ready(function () {
         event.preventDefault();
         var addedTitle = $("#bookTitle");
         var addedAuthor = $("#bookAuthor");
-        
+
         var bookAdded = {
             title: addedTitle.val().trim(),
             author: addedAuthor.val().trim(),
@@ -104,7 +104,7 @@ $(document).ready(function () {
         $("#btn-book").modal("hide");
 
         function addBook(title, author) {
-            $.post("/books/addedBooks", {
+            $.post("/books/addedbooks", {
                 user_id: userID,
                 title: title,
                 author: author
@@ -113,4 +113,152 @@ $(document).ready(function () {
             })
         }
     });
+    // ========================================================================
+
+    // CLICK FUNCTIONS FOR BUTTONS FOR ADDED ITEMS
+    // Function to run when any db-interaction button is clicked
+    function addedAction(nextupval, completedval, removedval, query) {
+
+        // Create object to send to database with 
+        var Obj = {
+            user_id: userID,
+            id: itemID,
+            nextup: nextupval,
+            completed: completedval,
+            remove: removedval
+        }
+
+        // Send post request to update user-added album/music/movies DB
+        $.post(query, Obj
+        ).then(function (data) {
+            console.log(data);
+            // If there's an error, log the error
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+
+    // NEXTUP BUTTON
+    $(".btn-nextup-add").on("click", function (event) {
+        event.preventDefault();
+
+        // Grab item ID
+        itemID = $(this).attr("id");
+
+        // Check to see where button was pressed
+        if ($(this).attr("data-site")) {
+            // If data-site is found, that means it's located in the sidebar/dashboard and the .item div should be hidden
+            $(this).closest(".item").fadeOut();
+
+        } else {
+            // It's in the main view and the table row should be removed
+            $(this).closest("tr").fadeOut(500, function () {
+                $(this).closest("tr").empty();
+            });
+        }
+
+        // Grab data-page value
+        var type = $(this).attr("data-page");
+
+        // Run switch statement to determine which parameters to pass to initialAction function
+        switch (type) {
+            case "music":
+                addedAction(true, false, false, "/music/addedalbums/update");
+                break;
+
+            case "movies":
+                addedAction(true, false, false, "/movies/addedmovies/update");
+                break;
+
+            case "books":
+                addedAction(true, false, false, "/books/addedbooks/update");
+                break;
+
+            default:
+                console.log("Something is wrong.")
+        }
+    })
+
+    // LISTENED TO BUTTON
+    $(".btn-completed-add").on("click", function (event) {
+        event.preventDefault();
+
+        // Grab item ID
+        itemID = $(this).attr("id");
+
+        // Check to see where button was pressed
+        if ($(this).attr("data-site")) {
+            // If data-site is found, that means it's located in the sidebar/dashboard and the .item div should be hidden
+            $(this).closest(".item").fadeOut();
+
+        } else {
+            // It's in the main view and the table row should be removed
+            $(this).closest("tr").fadeOut(500, function () {
+                $(this).closest("tr").empty();
+            });
+        }
+
+        // Grab data-page value
+        var type = $(this).attr("data-page");
+
+        // Run switch statement to determine which parameters to pass to initialAction function and run function
+        switch (type) {
+            case "music":
+                addedAction(false, true, false, "/music/addedalbums/update");
+                break;
+
+            case "movies":
+                addedAction(false, true, false, "/movies/addedmovies/update");
+                break;
+
+            case "books":
+                addedAction(false, true, false, "/books/addedbooks/update");
+                break;
+
+            default:
+                console.log("Something is wrong.")
+        }
+    })
+
+    // REMOVE BUTTON
+    $(".btn-remove-add").on("click", function (event) {
+        event.preventDefault();
+
+        // Grab item ID
+        itemID = $(this).attr("id");
+
+        // Check to see where button was pressed
+        if ($(this).attr("data-site")) {
+            // If data-site is found, that means it's located in the sidebar/dashboard and the .item div should be hidden
+            $(this).closest(".item").fadeOut();
+
+        } else {
+            // It's in the main view and the table row should be removed
+            $(this).closest("tr").fadeOut(500, function () {
+                $(this).closest("tr").empty();
+            });
+        }
+
+        // Grab data-page value
+        var type = $(this).attr("data-page");
+
+        // Run switch statement to determine which parameters to pass to initialAction function
+        switch (type) {
+            case "music":
+                addedAction(false, false, true, "/music/addedalbums/update");
+                break;
+
+            case "movies":
+                addedAction(false, false, true, "/movies/addedmovies/update");
+                break;
+
+            case "books":
+                addedAction(false, false, true, "/books/addedbooks/update");
+                break;
+
+            default:
+                console.log("Something is wrong.")
+        }
+    })
+    // ===========================================================================
 });
--- routes/user-added-api-routes.js
+++ routes/user-added-api-routes.js
@@ -1,15 +1,74 @@
 var db = require("../models");
 
 module.exports = function (app) {
-    
+
+    // GET REQUESTS FOR USER-ADDED CONTENT
+    // route to user added albums
+    app.get("/music/addedalbums", function (req, res) {
+        // query database to find all the added albums by loged in user
+        db.AlbumAdded.findAll({
+            where: {
+                user_id: req.user.id,
+                nextup: false,
+                completed: false,
+                remove: false
+            },
+            // takes me to wherever the album add list will be
+        }).then(function (data) {
+            var hbsObject = {
+                albums: data
+            };
+            res.render("music-added", hbsObject)
+        })
+    });
+
+    // route to user added albums
+    app.get("/movies/addedmovies", function (req, res) {
+        // query database to find all the added albums by loged in user
+        db.MovieAdded.findAll({
+            where: {
+                user_id: req.user.id,
+                nextup: false,
+                completed: false,
+                remove: false
+            },
+            // takes me to wherever the album add list will be
+        }).then(function (data) {
+            var hbsObject = {
+                title: data
+            };
+            res.render("movies-added", hbsObject)
+        })
+    });
+
+    // route to user added albums
+    app.get("/books/addedbooks", function (req, res) {
+        // query database to find all the added albums by loged in user
+        db.BookAdded.findAll({
+            where: {
+                user_id: req.user.id,
+                nextup: false,
+                completed: false,
+                remove: false
+            },
+            // takes me to wherever the album add list will be
+        }).then(function (data) {
+            var hbsObject = {
+                title: data
+            };
+            res.render("books-added", hbsObject)
+        })
+    });
+
+    // POST REQUSTS TO ADD USER ITEM===========================================
     // add an album to AlbumAdded
-    app.post("/music/addedAlbums",function(req, res){
+    app.post("/music/addedalbums", function (req, res) {
 
         console.log(req.body.user_id)
         console.log(req.body.album);
         console.log(req.body.artist);
         console.log(req.body.year);
-        
+
         db.AlbumAdded.create({
             user_id: req.body.user_id,
             album: req.body.album,
@@ -21,13 +80,13 @@ module.exports = function (app) {
     });
 
     // add a movie to MovieAdded
-    app.post("/movies/addedMovies",function(req, res){
+    app.post("/movies/addedmovies", function (req, res) {
 
         console.log(req.body.user_id)
         console.log(req.body.title);
         console.log(req.body.rating);
         console.log(req.body.year);
-        
+
         db.MovieAdded.create({
             user_id: req.body.user_id,
             title: req.body.title,
@@ -39,12 +98,12 @@ module.exports = function (app) {
     });
 
     // add a book to BookAdded
-    app.post("/books/addedBooks",function(req, res){
+    app.post("/books/addedbooks", function (req, res) {
 
         console.log(req.body.user_id)
         console.log(req.body.title);
         console.log(req.body.author);
-        
+
         db.BookAdded.create({
             user_id: req.body.user_id,
             title: req.body.title,
@@ -53,4 +112,51 @@ module.exports = function (app) {
             res.end();
         })
     });
+    // =====================================================================
+
+    // POST REQUESTS TO UPDATE USER-ADDED ITEMS
+    // Update user-added album with nextup, finished or removed values
+    app.post("/music/addedalbums/update", function (req, res) {
+        db.AlbumAdded.update(
+            req.body,
+            {
+                where: {
+                    id: req.body.id,
+                    user_id: req.body.user_id
+                }
+            }
+        ).then(function (dbCreate) {
+            res.json(dbCreate);
+        });
+    })
+
+    // Update user-added movie with nextup, finished or removed values
+    app.post("/movies/addedmovies/update", function (req, res) {
+        db.MovieAdded.update(
+            req.body,
+            {
+                where: {
+                    id: req.body.id,
+                    user_id: req.body.user_id
+                }
+            }
+        ).then(function (dbCreate) {
+            res.json(dbCreate);
+        });
+    })
+
+    // Update user-added books with nextup, finished or removed values
+    app.post("/books/addedbooks/update", function (req, res) {
+        db.BookAdded.update(
+            req.body,
+            {
+                where: {
+                    id: req.body.id,
+                    user_id: req.body.user_id
+                }
+            }
+        ).then(function (dbCreate) {
+            res.json(dbCreate);
+        });
+    })
 }
